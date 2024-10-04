e0185c9363b28f4f12aec9a659e8d8db10ecd842
Małgorzata Palewicz <gosiapalewicz@MacBook-Air-Gosia-2.local>
2024-10-04 15:10:44 +0200

    Initial files

---
+++ main.cpp
@@ -0,0 +1,100 @@
+/*********************************************
+Kod stanowi uzupe�nienie materia��w do �wicze�
+w ramach przedmiotu metody optymalizacji.
+Kod udost�pniony na licencji CC BY-SA 3.0
+Autor: dr in�. �ukasz Sztangret
+Katedra Informatyki Stosowanej i Modelowania
+Akademia G�rniczo-Hutnicza
+Data ostatniej modyfikacji: 19.09.2023
+*********************************************/
+
+#include"opt_alg.h"
+
+void lab0();
+void lab1();
+void lab2();
+void lab3();
+void lab4();
+void lab5();
+void lab6();
+
+int main()
+{
+	try
+	{
+		lab0();
+	}
+	catch (string EX_INFO)
+	{
+		cerr << "ERROR:\n";
+		cerr << EX_INFO << endl << endl;
+	}
+	system("pause");
+	return 0;
+}
+
+void lab0()
+{
+	//Funkcja testowa
+	double epsilon = 1e-2;
+	int Nmax = 10000;
+	matrix lb(2, 1, -5), ub(2, 1, 5), a(2, 1);
+	solution opt;
+	a(0) = -1;
+	a(1) = 2;
+	opt = MC(ff0T, 2, lb, ub, epsilon, Nmax, a); //funkcja celu, jej nazwa jest jednoczenise jej adresem, wymiar, ...,
+    // epsioln - pierwsze kruyterium zarzymania, Nmax - drugie kryteruym  zatrzymania, a - wposlczynnnik co podaj uzytkownik
+	cout << opt << endl << endl;
+	solution::clear_calls();
+
+	//Wahadlo
+	Nmax = 1000;
+	epsilon = 1e-2;
+	lb = 0; //lower bound
+	ub = 5; //upper bound
+	double teta_opt = 1; //1 radian = okolo 57 stopni
+	opt = MC(ff0R, 1, lb, ub, epsilon, Nmax, teta_opt);
+	cout << opt << endl << endl;
+	solution::clear_calls();
+
+	//Zapis symulacji do pliku csv
+	matrix Y0 = matrix(2, 1), MT = matrix(2, new double[2]{ m2d(opt.x),0.5 });
+	matrix* Y = solve_ode(df0, 0, 0.1, 10, Y0, NAN, MT);
+	ofstream Sout("symulacja_lab0.csv");
+	Sout << hcat(Y[0], Y[1]);
+	Sout.close();
+	Y[0].~matrix();
+	Y[1].~matrix();
+}
+
+void lab1()
+{
+
+}
+
+void lab2()
+{
+
+}
+
+void lab3()
+{
+
+}
+
+void lab4()
+{
+
+}
+
+void lab5()
+{
+
+}
+
+void lab6()
+{
+
+}
+
+//szukamy minimum + dla rzeczywisyefo obiektu (opisanego rownaniem rozniczkowym)
---
+++ user_funs.cpp
@@ -0,0 +1,33 @@
+#include"user_funs.h"
+
+matrix ff0T(matrix x, matrix ud1, matrix ud2){ //funkcja testowa
+	matrix y;
+	y = pow(x(0) - ud1(0), 2) + pow(x(1) - ud1(1), 2);
+	return y;
+}
+
+matrix ff0R(matrix x, matrix ud1, matrix ud2){ //funkcja problemu rzeczywistego , funckja celu
+	matrix y;
+	matrix Y0 = matrix(2, 1), MT = matrix(2, new double[2]{ m2d(x),0.5 }); //Y0 - poczatkowy uklad wahadla
+    // MT - moment sily i czas
+	matrix* Y = solve_ode(df0, 0, 0.1, 10, Y0, ud1, MT); //dwuleementoea tablica ktora zawiera dwia macierze, pierwsza z czasem
+    // a druga tablica zawiera dwie kolumny cztli y1 i y2, polozenie i jego predkosc
+	int n = get_len(Y[0]);
+	double teta_max = Y[1](0, 0); //maksymalne wychylenie
+	for (int i = 1; i < n; ++i)
+		if (teta_max < Y[1](i, 0))
+			teta_max = Y[1](i, 0);
+	y = abs(teta_max - m2d(ud1));
+	Y[0].~matrix();
+	Y[1].~matrix();
+	return y;
+}
+
+matrix df0(double t, matrix Y, matrix ud1, matrix ud2){
+	matrix dY(2, 1); //dwuwymiarowy
+	double m = 1, l = 0.5, b = 0.5, g = 9.81; //masa
+	double I = m*pow(l, 2); //dlugosc?
+	dY(0) = Y(1); //pochodna z y1
+	dY(1) = ((t <= ud2(1))*ud2(0) - m*g*l*sin(Y(0)) - b*Y(1)) / I; //(moment sily - czas dzialania sily) / moment bezwladnosci
+	return dY;
+}
